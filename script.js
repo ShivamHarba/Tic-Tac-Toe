fecb20637a9c7c69a78827ae88b51a85863bd449
ShivamHarba <shivamharba.nbnssoe.comp@gmail.com>
2025-01-10 21:49:09 +0530

    code add

--- script.js
+++ script.js
@@ -1,2 +1,39 @@
-const btn=document.querySelector(".btn1");
+const boxes = document.querySelectorAll(".box");
+
+console.log(boxes)
+
 const reset=document.querySelector(".reset");
+
+let turnX=true;
+
+const winners=[
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8],
+
+];
+
+
+// btn.addEventlistner("click",()=>{
+//     console.log("Hello")
+// })
+
+boxes.forEach((btn)=>{
+    btn.addEventListener("click", ()=>{
+        console.log("Hiii");
+        
+    })
+    
+})
+
+
+//  boxes.forEach((box)=> {
+//     box.addEventlistner("Click",()=>{
+//         console.log("cLick");
+//     });
+// });
